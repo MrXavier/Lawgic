91ec04bd369b096372b0e445766411c100633dc2
Raghuveer <raghuveer@localhost>
2024-11-07 14:31:37 +0100

    Lang chain completion method

--- backend/llm_client.py
+++ backend/llm_client.py
@@ -1,6 +1,8 @@
-from typing import Dict, List
 import os
+from typing import Dict, List
+
 from dotenv import load_dotenv
+from langchain_google_vertexai import ChatVertexAI
 
 load_dotenv()
 
@@ -14,6 +16,29 @@ def get_llm_completion(messages: List[Dict]) -> str:
         }
         
         return response["completion"]
+        
+    except Exception as e:
+        print(f"Error getting LLM completion: {str(e)}")
+        return ""
+
+def get_lchain_llm_completion(messages: List[Dict]) -> str:
+    try:
+        input_messages = [
+            (
+                "system",
+                "You are a helpful assistant that translates English to French. Translate the user sentence.",
+            ),
+            ("human", "I love programming."),
+        ]
+        chatbot = ChatVertexAI(
+            model="gemini-1.5-flash-001",
+            temperature=0,
+            max_tokens=None,
+            max_retries=6,
+            stop=None,
+        )
+        completion = chatbot.get_completion(input_messages)
+        return completion
         
     except Exception as e:
         print(f"Error getting LLM completion: {str(e)}")
